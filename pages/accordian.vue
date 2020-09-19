e3f425943038c903bace8b35939da1e092c5c2c3
Sakshi <sakshi.shirsekar20@nmims.edu.in>
2020-09-19 18:42:28 +0530

    added accordian

---
+++ pages/accordian.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-expansion-panels>
+    <v-expansion-panel
+      v-for="(item,a) in 1"
+      :key="i"
+    >
+      <v-expansion-panel-header>Item</v-expansion-panel-header>
+      <v-expansion-panel-content>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </v-expansion-panel>
+    <v-expansion-panel
+      v-for="(item,b) in 1"
+      :key="i"
+    >
+      <v-expansion-panel-header>Item</v-expansion-panel-header>
+      <v-expansion-panel-content>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+    <v-expansion-panel
+      v-for="(item,c) in 1"
+      :key="i"
+    >
+      <v-expansion-panel-header>Item</v-expansion-panel-header>
+      <v-expansion-panel-content>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+    
+  </v-expansion-panels>
+</template>
+
