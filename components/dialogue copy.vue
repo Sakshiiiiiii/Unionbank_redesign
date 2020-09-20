9bf2f4fbe6063a64a5beeaf14888b8913a53974a
Sakshi <sakshi.shirsekar20@nmims.edu.in>
2020-09-21 02:04:41 +0530

    Create dialogue copy.vue

---
+++ components/dialogue copy.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" scrollable max-width="300px">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Open Dialog
+        </v-bt\"height: 300px;">
+          <v-radio-group v-model="dialogm1" column>
+            <v-radio label="New User, The" value="New User"></v-radio>
+            <v-radio label="Existing User" value="Existing User"></v-radio>
+            
+          </v-radio-group>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        dialogm1: '',
+        dialog: false,
+      }
+    },
+  }
+</script>
