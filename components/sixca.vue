d499f5a1be7f76fadf3037fecfdcbe758948a1d6
Sakshi <sakshi.shirsekar20@nmims.edu.in>
2020-11-22 15:42:25 +0530

    componets added

---
+++ components/sixca.vue
@@ -0,0 +1,26 @@
+   <template>
+   <v-card 
+        color="white"
+        class="mx-auto"
+        max-width="500"
+        >
+ <v-img
+          md="6" sm="6"
+          contain="false"
+          class="mx-auto"
+          max-height="300px"
+          max-width="400"
+          src="Onlinepayment.png">
+        </v-img>
+<v-btn md="6" sm="6"
+                  block="true"
+                  max-width="400"
+                  text="true"
+                  color="blue darken-2"
+                  dark
+                  v-bind="attrs"
+                  v-on="on">
+                  Online Payment
+                </v-btn>
+     </v-card>   
+     </template> 
