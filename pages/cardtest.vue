c09a4acd872346f9b1baa36d0adb63f504b9b4a4
radhaavaidya <60287291+radhaavaidya@users.noreply.github.com>
2020-11-29 13:25:24 +0530

    added comp

---
+++ pages/cardtest.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <v-content>
+      <v-container>
+        <v-row justify="center">
+          <cardtest/>
+        </v-row>
+      </v-container>
+    </v-content>
+  </div>
+</template>
+
