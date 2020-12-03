781187c21f717159367a2ed6852c417d67b2da1c
sparshags <60287208+sparshags@users.noreply.github.com>
2020-12-03 23:05:41 +0530

    saks

--- components/Cards1.vue
+++ components/Cards1.vue
@@ -8,19 +8,23 @@
         <!--the hover function is defined and given its properties  -->
 
   <v-card
+  
   :elevation="hover ? 16 : 2"
           :class="{ 'on-hover': hover }"
           class="mx-auto"
           max-width="1200"
-          color="#195289">
+          color="#FFFFFF">
+          <v-alert outlined color="#195289">
     
     <!-- paragraph tag is used to diffrentiate between the header and te rest of the text --> 
     <!-- another component is table which is anoter component-->
-    <v-card-text>
+    <v-card-text color="000000">
             <p class="display-1 text--primary">
         Features Of SBGEN
       </p>
+
             <div class="text--primary">
+              <p>
        Any Resident Individual - Single Accounts, Two or 
 more individuals in Joint Accounts, Illiterate Persons, 
 Visually Impaired persons, Purdanasheen Ladies,
@@ -28,15 +32,19 @@ Minors, Associations, Clubs, Societies, etc. <br>
 
 Minimum balance requirement with or without 
 cheque book facility:<br>
+</p>
 <Table/>
 
       </div>
     </v-card-text>
-    
+    </v-alert>
   </v-card>
   </v-hover>
 </template>
 <script>
+p { color= "#000000" 
+
+}
   export default {
   }
   
